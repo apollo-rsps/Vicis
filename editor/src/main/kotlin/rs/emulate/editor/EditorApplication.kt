c002b3101498f892b1c4dbf3edcb3fee73a9b589
Gary Tierney <gary.tierney@gmx.com>
2018-04-22 04:56:17 +0100

    Add SCSS stylesheets and some basic styling

--- editor/src/main/kotlin/rs/emulate/editor/EditorApplication.kt
+++ editor/src/main/kotlin/rs/emulate/editor/EditorApplication.kt
@@ -3,7 +3,12 @@ package rs.emulate.editor
 import rs.emulate.editor.startup.EditorStartupView
 import tornadofx.*
 
-class EditorApplication : App(EditorStartupView::class)
+class EditorApplication : App(EditorStartupView::class) {
+    init {
+        importStylesheet("/styles.css")
+        reloadStylesheetsOnFocus()
+    }
+}
 
 fun main(args: Array<String>) {
     launch<EditorApplication>()
