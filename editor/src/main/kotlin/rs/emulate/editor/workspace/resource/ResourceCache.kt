ef4dc182b66d98f9a6fb3c2d4ee3bf15e8d204f2
Major- <major@emulate.rs>
2018-05-06 01:58:30 +0100

    Separate config resources and viewers

--- editor/src/main/kotlin/rs/emulate/editor/workspace/resource/ResourceCache.kt
+++ editor/src/main/kotlin/rs/emulate/editor/workspace/resource/ResourceCache.kt
@@ -3,7 +3,7 @@ package rs.emulate.editor.workspace.resource
 import rs.emulate.editor.workspace.resource.index.ResourceIndexBuilder
 
 class ResourceCache(val bundles: List<ResourceBundle<*>> = emptyList()) {
-    val indexTypeMap = bundles.associateBy { it.idType }
+    val indexTypeMap = bundles.associateBy(ResourceBundle<*>::idType)
 
     fun index() = bundles.fold(ResourceIndexBuilder()) { builder, bundle ->
         bundle.index(builder)
@@ -11,6 +11,7 @@ class ResourceCache(val bundles: List<ResourceBundle<*>> = emptyList()) {
     }.build()
 
     fun <T: ResourceId> load(resourceId: T): Resource? {
+        @Suppress("UNCHECKED_CAST")
         val resourceBundle = indexTypeMap[resourceId::class] as ResourceBundle<T>?
         return resourceBundle?.load(resourceId)
     }
--- editor/src/main/kotlin/rs/emulate/editor/workspace/resource/bundles/legacy/config/ConfigResource.kt
+++ editor/src/main/kotlin/rs/emulate/editor/workspace/resource/bundles/legacy/config/ConfigResource.kt
@@ -5,40 +5,49 @@ import javafx.collections.ObservableMap
 import rs.emulate.editor.workspace.resource.Resource
 import rs.emulate.editor.workspace.resource.ResourceBundle
 import rs.emulate.editor.workspace.resource.ResourceId
+import rs.emulate.legacy.IndexedFileSystem
 import rs.emulate.legacy.config.ConfigPropertyType
 import rs.emulate.legacy.config.MutableConfigDefinition
 
 /**
  * A [Resource] from the config archive.
  */
-class ConfigResource<T : MutableConfigDefinition>(override val id: ResourceId, definition: T) : Resource {
+abstract class ConfigResource<T : MutableConfigDefinition>(override val id: ResourceId, definition: T) : Resource {
 
     val properties: ObservableMap<ConfigPropertyType<*>, Any?> = FXCollections.observableMap(
         definition.serializableProperties().associateBy({ it.value.type }, { it.value.value })
     )
 
     override fun equals(other: Any?): Boolean {
-        return other is ConfigResource<*> && id == other.id
+        return other is ConfigResource<*> && id == other.id && properties == other.properties
     }
 
     override fun hashCode(): Int {
         return 31 * properties.hashCode() + id.hashCode()
     }
 
-    override fun toString(): String {
-        return "ConfigResource($id) { $properties }"
-    }
-
     companion object {
-        const val INDEX_CATEGORY = "Config"
+        const val INDEX_CATEGORY_NAME = "Config"
     }
 
 }
 
 interface ConfigResourceBundle<I : ConfigResourceId, T : MutableConfigDefinition> : ResourceBundle<I> {
-
     val definitions: Map<I, T>
+}
 
-    override fun load(id: I): Resource = ConfigResource(id, definitions[id]!!)
+class ConfigResourceBundleFactory(fs: IndexedFileSystem) {
 
+    private val config = fs.getArchive(CONFIG_INDEX, CONFIG_ARCHIVE_ID)
+
+    val items = ItemResourceBundle(config)
+    val npcs = NpcResourceBundle(config)
+    val objects = ObjectResourceBundle(config)
+
+    fun bundles(): List<ResourceBundle<*>> = listOf(items, npcs, objects)
+
+    private companion object {
+        const val CONFIG_INDEX = 0
+        const val CONFIG_ARCHIVE_ID = 2
+    }
 }
