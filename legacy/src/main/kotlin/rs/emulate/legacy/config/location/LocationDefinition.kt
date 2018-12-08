08210f6be13cc60260d42240901742b7e35fc627
Major- <major@emulate.rs>
2018-12-08 02:29:44 +0000

    Replace LocationDefinition with mutable variant

---
+++ legacy/src/main/kotlin/rs/emulate/legacy/config/location/LocationDefinition.kt
@@ -0,0 +1,40 @@
+package rs.emulate.legacy.config.location
+
+import rs.emulate.legacy.config.npc.MorphismSet
+
+class LocationDefinition(
+    val id: Int,
+    var models: IntArray = IntArray(0),
+    var modelTypes: IntArray = IntArray(0),
+    var name: String = "null",
+    var description: String = "",
+    var width: Int = 0,
+    var length: Int = 0,
+    var solid: Boolean = true,
+    var impenetrable: Boolean = true,
+    var interactive: Int = -1,
+    var contourGround: Boolean = false,
+    var delayShading: Boolean = false,
+    var occludes: Boolean = false,
+    var sequenceId: Int = 0,
+    var decorDisplacement: Int = 0,
+    var brightness: Int = 0,
+    var diffusion: Int = 0,
+    var actions: Array<String?> = arrayOfNulls(10),
+    var colours: MutableMap<Int, Int> = mutableMapOf(),
+    var minimapFunction: Int = 0,
+    var inverted: Boolean = false,
+    var castShadow: Boolean = true,
+    var scaleX: Int = 0,
+    var scaleY: Int = 0,
+    var scaleZ: Int = 0,
+    var mapsceneId: Int = 0,
+    var surroundings: Int = 0,
+    var translateX: Int = 0,
+    var translateY: Int = 0,
+    var translateZ: Int = 0,
+    var obstructsGround: Boolean = false,
+    var hollow: Boolean = false,
+    var supportsItems: Int = if (solid) 1 else -1,
+    var morphisms: MorphismSet = MorphismSet.EMPTY
+)
---
+++ legacy/src/main/kotlin/rs/emulate/legacy/config/location/LocationDefinitionDecoder.kt
@@ -0,0 +1,100 @@
+package rs.emulate.legacy.config.location
+
+import rs.emulate.legacy.config.ConfigDecoder
+import rs.emulate.legacy.config.npc.MorphismSet
+import rs.emulate.shared.util.DataBuffer
+
+object LocationDefinitionDecoder : ConfigDecoder<LocationDefinition> {
+
+    override val entryName: String = "loc"
+
+    override fun decode(id: Int, buffer: DataBuffer): LocationDefinition {
+        val definition = LocationDefinition(id)
+        var opcode = buffer.getUnsignedByte()
+
+        while (opcode != ConfigDecoder.DEFINITION_TERMINATOR) {
+            definition.decode(buffer, opcode)
+            opcode = buffer.getUnsignedByte()
+        }
+
+        definition.apply {
+            if (interactive == -1) {
+                interactive = if (actions.isNotEmpty()) {
+                    1
+                } else if (models.isNotEmpty() && (modelTypes.isEmpty() || modelTypes.first() == 10)) 1 else 0
+            }
+
+            if (hollow) {
+                solid = false
+                impenetrable = false
+            }
+
+            if (supportsItems == -1) {
+                supportsItems = if (solid) 1 else 0
+            }
+        }
+
+        return definition
+    }
+
+    private fun LocationDefinition.decode(buffer: DataBuffer, opcode: Int) {
+        when (opcode) {
+            1 -> {
+                val count = buffer.getUnsignedByte()
+                models = IntArray(count)
+                modelTypes = IntArray(count)
+
+                repeat(count) { index ->
+                    models[index] = buffer.getUnsignedShort()
+                    modelTypes[index] = buffer.getUnsignedByte()
+                }
+            }
+            2 -> name = buffer.getString()
+            3 -> description = buffer.getString()
+            5 -> {
+                val count = buffer.getUnsignedByte()
+                models = IntArray(count) { buffer.getUnsignedShort() }
+            }
+            14 -> width = buffer.getUnsignedByte()
+            15 -> length = buffer.getUnsignedByte()
+            17 -> solid = false
+            18 -> impenetrable = false
+            19 -> interactive = buffer.getUnsignedByte()
+            21 -> contourGround = true
+            22 -> delayShading = true
+            23 -> occludes = true
+            24 -> sequenceId = buffer.getUnsignedShort().let { if (it == 65535) -1 else it }
+            28 -> decorDisplacement = buffer.getUnsignedByte()
+            29 -> brightness = buffer.getByte()
+            in 30 until 39 -> actions[opcode - 30] = buffer.getString().let { if (it == "hidden") null else it }
+            39 -> diffusion = buffer.getByte()
+            40 -> {
+                val count = buffer.getUnsignedByte()
+
+                repeat(count) {
+                    val original = buffer.getUnsignedShort()
+                    val replacement = buffer.getUnsignedShort()
+
+                    colours[original] = replacement
+                }
+            }
+            60 -> minimapFunction = buffer.getUnsignedShort()
+            62 -> inverted = true
+            64 -> castShadow = false
+            65 -> scaleX = buffer.getUnsignedShort()
+            66 -> scaleY = buffer.getUnsignedShort()
+            67 -> scaleZ = buffer.getUnsignedShort()
+            68 -> mapsceneId = buffer.getUnsignedShort()
+            69 -> surroundings = buffer.getUnsignedByte()
+            70 -> translateX = buffer.getShort()
+            71 -> translateY = buffer.getShort()
+            72 -> translateZ = buffer.getShort()
+            73 -> obstructsGround = true
+            74 -> hollow = true
+            75 -> supportsItems = buffer.getUnsignedByte()
+            77 -> morphisms = MorphismSet.decode(buffer)
+        }
+    }
+
+}
+
