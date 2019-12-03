1d3cace3a357b562c338117d097d671e361db494
Major <major@emulate.rs>
2019-12-03 01:55:43 +0000

    Remove module-infos

--- modern/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module rs.emulate.modern {
-	requires transitive rs.emulate.util;
-	requires kotlin.stdlib;
-
-	exports rs.emulate.modern;
-	exports rs.emulate.modern.compression;
-	exports rs.emulate.modern.fs;
-}
