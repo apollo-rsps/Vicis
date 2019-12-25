6d62f4628c1eea5e85ab186e29259d7853f197ce
Major <major@emulate.rs>
2019-12-25 05:20:18 +0000

    Capitalise NPC property categories

--- editor/src/main/kotlin/rs/emulate/editor/legacy/npc/NpcProperty.kt
+++ editor/src/main/kotlin/rs/emulate/editor/legacy/npc/NpcProperty.kt
@@ -10,127 +10,127 @@ sealed class NpcProperty(
 
     object Models : NpcProperty(
         "Models",
-        category = "model",
+        category = "Model",
         description = "The model ids composed to create the npc."
     )
 
     object Name : NpcProperty(
         "Name",
-        category = "text",
+        category = "Text",
         description = "The name displayed on hover, right click, etc."
     )
 
     object Description : NpcProperty(
         "Description",
-        category = "text",
+        category = "Text",
         description = "The text shown when examining the npc."
     )
 
     object Size : NpcProperty(
         "Size",
-        category = "model",
+        category = "Model",
         description = "The size, in tiles, of the npc."
     )
 
     object StandingSequence : NpcProperty(
         "Standing animation",
-        category = "animations",
+        category = "Animations",
         description = "The animation performed when the npc is standing idle."
     )
 
     object WalkingSequence : NpcProperty(
         "Walking animation",
-        category = "animations",
+        category = "Animations",
         description = "The animation performed when the npc is walking."
     )
 
     object MovementSequences : NpcProperty(
         "Movement animations",
-        category = "animations",
+        category = "Animations",
         description = "Set of animations performed when the npc performs different movements."
     )
 
     object Actions : NpcProperty(
         "Actions",
-        category = "interaction",
+        category = "Interaction",
         description = "The context menu actions."
     )
 
     object Colours : NpcProperty(
         "Colours",
-        category = "model",
+        category = "Model",
         description = "The model colours to replace for this npc."
     )
 
     object WidgetModels : NpcProperty(
         "Widget models",
-        category = "model",
+        category = "Model",
         description = "The model ids composed to display the npc on a widget."
     )
 
     object VisibleOnMinimap : NpcProperty(
         "Visible on minimap",
-        category = "display",
+        category = "Display",
         description = "Whether or not to display a yellow dot on the client minimap for this npc."
     )
 
     object CombatLevel : NpcProperty(
         "Combat level",
-        category = "text",
+        category = "Text",
         description = "The combat level displayed on right click (0 displays no level)."
     )
 
     object PlanarScale : NpcProperty(
         "Planar model scale",
-        category = "display",
+        category = "Display",
         description = "The scale factor to apply with respect to the plane (width/depth)."
     )
 
     object VerticalScale : NpcProperty(
         "Vertical model scale",
-        category = "display",
+        category = "Display",
         description = "The scale factor to apply vertically."
     )
 
     object PriorityRender : NpcProperty(
         "High-priority render",
-        category = "display",
+        category = "Display",
         description = "Whether or not this npc should be rendered before other npcs."
     )
 
     object Brightness : NpcProperty(
         "Brightness",
-        category = "display",
+        category = "Display",
         description = "The brightness offset to apply when lighting this npc's model."
     )
 
     object Diffusion : NpcProperty(
         "Diffusion",
-        category = "display",
+        category = "Display",
         description = "The diffusion offset to apply when lighting this npc's model."
     )
 
     object HeadIcon : NpcProperty(
         "Prayer head icon",
-        category = "display",
+        category = "Display",
         description = "The id of the prayer headicon to draw above this npc."
     )
 
     object DefaultOrientation : NpcProperty(
         "Default orientation",
-        category = "display",
+        category = "Display",
         description = "The default orientation of the npc model."
     )
 
     object Morphisms : NpcProperty(
         "Morphisms",
-        category = "morphisms",
+        category = "Morphing",
         description = "The npc ids this npc can morph into."
     )
 
     object Clickable : NpcProperty(
         "Clickable",
-        category = "interaction",
+        category = "Interaction",
         description = "Whether or not this npc can be clicked by a player."
     )
 
