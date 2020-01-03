f4dbbf0a473863e4074ca9de65449941f0b2d63a
Major <major@emulate.rs>
2020-01-03 16:39:10 +0000

    Update JavaFX plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     base
     kotlin("jvm") version "1.3.61" apply false
-    id("org.openjfx.javafxplugin") version "0.0.5" apply false
+    id("org.openjfx.javafxplugin") version "0.0.8" apply false
     id("com.github.ben-manes.versions") version "0.27.0"
 }
 
