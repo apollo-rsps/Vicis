8cbac99ffa028d161fa77ba2f833ee4a3ac26bb3
Gary Tierney <gary.tierney@fastmail.com>
2020-03-07 02:24:08 +0000

    Set up publishing to Bintray

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,6 @@
-import org.gradle.jvm.tasks.Jar
+import com.jfrog.bintray.gradle.BintrayExtension
+import com.jfrog.bintray.gradle.BintrayPlugin
+import org.jetbrains.kotlin.gradle.plugin.KotlinPluginWrapper
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -6,11 +8,11 @@ plugins {
     kotlin("jvm") version "1.3.61" apply false
     id("org.openjfx.javafxplugin") version "0.0.8" apply false
     id("com.github.ben-manes.versions") version "0.27.0"
+    id("com.jfrog.bintray") version "1.8.4" apply (false)
 }
 
 allprojects {
     group = "rs.emulate"
-    version = "1.0"
 
     repositories {
         mavenCentral()
@@ -22,16 +24,50 @@ allprojects {
 
 subprojects {
     afterEvaluate {
-        dependencies {
-            add("compile", "com.michael-bull.kotlin-result:kotlin-result:1.1.4")
+        plugins.withType<KotlinPluginWrapper> {
+            dependencies {
+                val api by configurations
+                api("com.michael-bull.kotlin-result:kotlin-result:1.1.4")
+            }
+        }
 
-            add("testImplementation", kotlin("test"))
-            add("testImplementation", "org.junit.jupiter:junit-jupiter-api:5.5.2")
-            add("testImplementation", "org.junit.jupiter:junit-jupiter-params:5.5.2")
-            add("testImplementation", "org.junit.jupiter:junit-jupiter-api:5.5.2")
-            add("testImplementation", "io.mockk:mockk:1.7.15")
+        plugins.withType<BintrayPlugin> {
+            configure<BintrayExtension> {
+                user = System.getenv("BINTRAY_USER")
+                key = System.getenv("BINTRAY_KEY")
+                publish = true
+                setPublications("maven")
 
-            add("testRuntimeOnly", "org.junit.jupiter:junit-jupiter-engine:5.5.2")
+                pkg(delegateClosureOf<BintrayExtension.PackageConfig> {
+                    name = rootProject.name
+                    userOrg = "apollo-rsps"
+                    desc = description
+                    repo = property("bintray.repository") as String
+
+                    setLicenses("ISC")
+                })
+            }
+        }
+
+        plugins.withType<MavenPublishPlugin> {
+            configure<PublishingExtension> {
+                publications.create<MavenPublication>("maven") {
+                    from(components["java"])
+
+                    groupId = project.group as String
+                    artifactId = project.name
+                    version = project.version as String
+                }
+            }
+        }
+
+        plugins.withType<JavaPlugin> {
+            configure<JavaPluginExtension> {
+                withSourcesJar()
+
+                sourceCompatibility = JavaVersion.VERSION_11
+                targetCompatibility = JavaVersion.VERSION_11
+            }
         }
 
         tasks.withType<Test> {
@@ -41,30 +77,9 @@ subprojects {
             }
         }
 
-        tasks.withType<JavaCompile>().configureEach {
-            val kotlinCompileTask = tasks.findByName("compileKotlin") as KotlinCompile
-
-            destinationDir = kotlinCompileTask.destinationDir
-            dependsOn(kotlinCompileTask)
-
-            doFirst {
-                options.compilerArgs = listOf("--module-path", classpath.asPath)
-            }
-        }
-
         tasks.withType<KotlinCompile>().configureEach {
-            println("Configuring $name in project ${project.name}...")
-
             kotlinOptions {
                 jvmTarget = "1.8"
-            }
-        }
-
-        tasks.withType<Jar> {
-            inputs.property("moduleName", ext["moduleName"])
-
-            manifest {
-                attributes("Automatic-Module-Name" to ext["moduleName"])
             }
         }
     }
--- util/build.gradle.kts
+++ util/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm")
     `java-library`
+    `maven-publish`
 }
 
 description = "Vicis Utilities"
